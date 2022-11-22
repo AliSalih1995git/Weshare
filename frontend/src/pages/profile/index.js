c035cc6faaabb82d5af9351bc29287229c4725c6
AliSalih1995git <kvaliswalihtnl@gmail.com>
2022-11-22 12:05:51 +0530

    ProfileMenu created

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -5,12 +5,14 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { profileReducer } from '../../functions/reducer';
 import Header from '../../components/header';
 import './style.css';
+import Cover from './Cover';
+import ProfilePictureInfos from './ProfilePictureInfos';
+import ProfileMenu from './ProfileMenu';
 
 function Profile() {
   const navigate = useNavigate();
   const { username } = useParams();
   const { user } = useSelector((state) => ({ ...state }));
-  const [showCoverMenu, setShowCoverMeun] = useState(true);
   var userName = username === undefined ? user.username : username;
   const [{ loading, error, profile }, dispatch] = useReducer(profileReducer, {
     loading: false,
@@ -23,7 +25,7 @@ function Profile() {
   const getProfile = async () => {
     try {
       dispatch({
-        type: 'PROFILE_REQUEST',
+        type: "PROFILE_REQUEST",
       });
       const { data } = await axios.get(
         `${process.env.REACT_APP_BACKEND_URL}/getProfile/${userName}`,
@@ -34,16 +36,16 @@ function Profile() {
         }
       );
       if (data.ok === false) {
-        navigate('/profile');
+        navigate("/profile");
       } else {
         dispatch({
-          type: 'PROFILE_SUCCESS',
+          type: "PROFILE_SUCCESS",
           payload: data,
         });
       }
     } catch (error) {
       dispatch({
-        type: 'PROFILE_ERROR',
+        type: "PROFILE_ERROR",
         payload: error.response.data.message,
       });
     }
@@ -53,32 +55,9 @@ function Profile() {
       <Header page="profile" />;
       <div className="profile_top">
         <div className="profile_container">
-          <div className="profile_cover">
-            {profile.cover && (
-              <img src={profile.cover} className="cover" alt="" />
-            )}
-            <div className="udpate_cover_wrapper">
-              <div
-                className="open_cover_update"
-                onClick={() => {
-                  setShowCoverMeun((prev) => !prev);
-                }}
-              >
-                <i className="camera_filled_icon"></i>
-                Add Cover photo
-              </div>
-              {showCoverMenu && <div className="open_cover_menu">
-                <div className="open_cover_menu_item">
-                  <i className="photo_icon"></i>
-                  Select Photo
-                </div>
-                <div className="open_cover_menu_item">
-                  <i className="upload_icon"></i>
-                  Upload Photo
-                </div>
-                </div>}
-            </div>
-          </div>
+      <Cover cover={profile.cover}/>
+      <ProfilePictureInfos profile={profile}/>
+      <ProfileMenu/>
         </div>
       </div>
     </div>
